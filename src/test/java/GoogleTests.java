89ea1878955860ce5360e28e4cbb4646e053712d
ruslanPonomarev <ruslan.ponomarev@wearebrain.com>
2020-12-14 14:03:51 +0200

    added GoogleTest

---
+++ src/test/java/GoogleTests.java
@@ -0,0 +1,23 @@
+import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selectors.byName;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class GoogleTests {
+
+  @Test
+  void selenideSearchTest() {
+    // Open google browser
+    open("https://google.com");
+
+    // Enter Selenide in search field
+    $(byName("q")).setValue("Selenide").pressEnter();
+
+    // Check that Selenide displays in search results
+    $("html").shouldHave(Condition.text("selenide.org"));
+    //Let's define a more precise scope for the assertion:
+    $("#rcnt").shouldHave(Condition.text("selenide.org"));
+  }
+}
